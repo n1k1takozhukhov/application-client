da4e34f5b12eb3cece84eb1202edc003dd02b5d0
n1k1takozhukhov <noloyaltyprod@gmail.com>
2024-09-23 21:31:34 +0500

    initial commit

---
+++ application-client/application-client/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  application-client
+//
+//  Created by Никита Кожухов on 23.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red //old
+        view.backgroundColor = .yellow
+
+    }
+
+
+}
+
---
+++ application-client/application-clientUITests/application_clientUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  application_clientUITestsLaunchTests.swift
+//  application-clientUITests
+//
+//  Created by Никита Кожухов on 23.09.2024.
+//
+
+import XCTest
+
+final class application_clientUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
